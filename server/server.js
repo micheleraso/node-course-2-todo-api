e2caa3c5e9901ba54641223169f1716ea60d0aed
Michele Raso <dev@micheleraso.com>
2017-08-22 17:23:37 +0200

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
     });
@@ -39,27 +39,15 @@ app.get('/todos/:id', (req, res) => {
     }
 
     Todo.findById(id).then((todo) => {
-        if(!todo) {
+        if (!todo) {
             return res.status(404).send();
-        } 
+        }
 
-        res.send({todo});
+        res.send({ todo });
     }).catch((e) => {
         res.status(400).send(e);
     });
-
-
-    // Valid id using isValid
-        // 404 - send back empty send
-
-    // findById
-        // success
-            // if todo - send it back
-            // if not todo - send back 404 with empty body
-        // error
-            // 400 - and send empty body back
-    });
-
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
